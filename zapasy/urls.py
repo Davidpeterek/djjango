25e9d6c73c55004dd155e84348769707393a4343
Davidpeterek <david.peterek@email.cz>
2024-06-10 20:52:47 +0200

    06

--- zapasy/urls.py
+++ zapasy/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import ZapasListView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('kluby/', views.KlubListView.as_view(), name='list.html'),
-    path('kluby/<int:pk>', views.KlubDetailView.as_view(), name='detail.html'),
-    path('zapas/', ZapasListView.as_view(), name='zapas_list.html'),
+    path('klub/<int:klub_id>/', views.klub_detail, name='detail.html'),
+    path('kluby/', views.klub_list, name='list.html'),
+    path('zapas/<int:zapas_id>/', views.zapas_detail, name='zapas_detail.html'),
+    path('zapasy/', views.zapas_list, name='zapas_list.html'),
+    path('hrac/<int:hrac_id>/', views.hrac_detail, name='hrac_detail.html'),
+    path('hraci/', views.hrac_list, name='hrac_list.html'),
 ]
--- zapasy/views.py
+++ zapasy/views.py
@@ -14,20 +14,26 @@ def index(request):
     return render(request, 'index.html', context=context)
 
 
-class KlubListView(ListView):
-    model = Klub
-    template_name ='kluby/list.html'
-    context_object_name ='kluby'
-    queryset = Klub.objects.order_by('nazev')
+def klub_detail(request, klub_id):
+    klub = Klub.objects.get(id=klub_id)
+    return render(request, 'kluby/detail.html', {'klub': klub})
 
+def klub_list(request):
+    kluby = Klub.objects.all()
+    return render(request, 'kluby/list.html', {'kluby': kluby})
 
-class KlubDetailView(DetailView):
-    model = Klub
-    template_name ='kluby/detail.html'
-    context_object_name ='klub'
+def zapas_detail(request, zapas_id):
+    zapas = Zapas.objects.get(id=zapas_id)
+    return render(request, 'zapas/zapas_detail.html', {'zapas': zapas})
 
-class ZapasListView(DetailView):
-    model = Zapas
-    template_name = 'zapas/zapas_list.html'
-    context_object_name = 'zapas'
-    ordering = ['-datum']
+def zapas_list(request):
+    zapas = Zapas.objects.all()
+    return render(request, 'zapas/zapas_list.html', {'zapasy': zapas})
+
+def hrac_detail(request, hrac_id):
+    hrac = Hrac.objects.get(id=hrac_id)
+    return render(request, 'hrac/hrac_detail.html', {'hrac': hrac})
+
+def hrac_list(request):
+    hraci = Hrac.objects.all()
+    return render(request, 'hrac/hrac_list.html', {'hraci': hraci})
